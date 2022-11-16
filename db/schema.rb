edabc69e50cb0be720d274504c0e66a80a006201
Kenechukwu Oguagbaka <kc101com@gmail.com>
2022-11-16 15:35:39 +0100

    migrate user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_21_153840) do
+ActiveRecord::Schema.define(version: 2022_11_16_143031) do
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.boolean "completed"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
